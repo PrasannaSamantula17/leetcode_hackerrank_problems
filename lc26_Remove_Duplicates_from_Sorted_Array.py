98eb2fc173e2ba185fdc9af52d303a9325f2f799
Prasanna <gowriprasanna3117@gmail.com>
2026-06-15 06:35:03 +0530

    Create lc26_Remove_Duplicates_from_Sorted_Array.py

---
+++ lc26_Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,14 @@
+# Time Complexity - O(n)
+# Space Complexity - O(1)
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        slow = 0
+        for fast in range(1,len(nums)):
+            if nums[slow]!=nums[fast]:
+                slow += 1
+                nums[slow] = nums[fast]
+        return slow+1
