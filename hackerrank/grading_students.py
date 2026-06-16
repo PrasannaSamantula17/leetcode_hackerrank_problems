1af81556bcfa081ef21fb4266e7248db0bd72e0e
Prasanna <gowriprasanna3117@gmail.com>
2026-06-16 08:23:11 +0530

    Create grading_students.py

---
+++ hackerrank/grading_students.py
@@ -0,0 +1,53 @@
+# Time Complexity - O(n)
+# Space Complexity - O(n)
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    result = []
+    for grade in grades:
+        # Rule: If grade is less than 38, no rounding occurs
+        if grade < 38:
+            result.append(grade)
+        else:
+            # Calculate next multiple of 5
+            next_multiple = ((grade // 5) + 1) * 5
+            
+            # If the difference is less than 3, round up
+            if next_multiple - grade < 3:
+                result.append(next_multiple)
+            else:
+                result.append(grade)
+    return result
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
