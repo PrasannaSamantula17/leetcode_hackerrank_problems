9bf91c7ba8fc74ac678b783ec9084647a244fe8a
Prasanna <gowriprasanna3117@gmail.com>
2026-06-17 07:05:07 +0530

    Create 881_boats_to_save_people.py

---
+++ leetcode/881_boats_to_save_people.py
@@ -0,0 +1,21 @@
+# Time Complexity - O(n log n)
+# Space Complexity - O(1)
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left, right = 0, len(people) - 1
+        boats = 0
+        
+        while left <= right:
+            # If the lightest and heaviest person can fit in one boat
+            if people[left] + people[right] <= limit:
+                left += 1
+            
+            # The heaviest person always gets a boat (either with the lightest or alone)
+            right -= 1
+            boats += 1
+            
+        return boats
+        
+        return res
